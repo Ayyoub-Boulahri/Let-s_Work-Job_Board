a63f12e18a87ed33ce873393bdf13f5a3e587fdc
AyoubPro44 <ayyoubboulahri@gmail.com>
2024-01-03 00:45:19 +0100

    project creation

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# Job_Board_PFE
+here is you will find all the libraries we will use :
+
+********************************************************
+- for the github collaboration here is the steps :
+    - git clone ....
+    - git pull origin master
+    - git checkout -b name_branche
+    - git add .
+    - git commit -m "message"
+    - git push origin name_branche
+    - then check the github
+    - create pull request
+    - merge pull request
+
+********************************************************
+
+- for the icons library here is the link : 
+    - https://react-icons.github.io/react-icons/
+
+********************************************************
+
+- for the database connection :
+    - host: 'sql3.freesqldatabase.com',
+    - user: 'sql3673884',
+    - password: 'DFqu1rDtlr',
+    - database: 'sql3673884',
+
+********************************************************
+
+- for the ui library we will use the radix ui here is the link for use it :
+    - https://www.radix-ui.com/themes/docs/components/button
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Navbar, NavbarMenuToggle, NavbarMenu, NavbarMenuItem, Link, NavbarBrand, NavbarContent, NavbarItem, Button } from "@nextui-org/react";
+import { useState } from 'react';
+
+function NavBar() {
+    const [isMenuOpen, setIsMenuOpen] = useState(true);
+
+
+    return (
+        <Navbar onMenuOpenChange={setIsMenuOpen} >
+            <NavbarContent>
+                <NavbarBrand>
+                    <p className="text-primary-500 font-extrabold text-lg text-[24px] cursor-pointer">Let's Work</p>
+                </NavbarBrand>
+            </NavbarContent>
+
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Home
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        About
+                    </Link>
+                </NavbarItem>
+                <NavbarItem>
+                    <Link color="foreground" href="#">
+                        Contact
+                    </Link>
+                </NavbarItem>
+            </NavbarContent>
+            <NavbarContent justify="end">
+                <NavbarItem className="hidden lg:flex">
+                </NavbarItem>
+                <NavbarItem>
+                    <Button color="primary" href="#" variant="flat">
+                        Sign Up
+                    </Button>
+                </NavbarItem>
+            </NavbarContent>
+        </Navbar>
+    )
+}
+
+export default NavBar
