d3fdd86cbe47bc815b00d1bad609cf7e6aa937b6
AyoubPro44 <ayyoubboulahri@gmail.com>
2024-01-03 13:54:40 +0100

    config styling

--- frontend/src/pages/acceuil.jsx
+++ frontend/src/pages/acceuil.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import "../css/acceuil.css"
 import { IoAirplaneSharp } from "react-icons/io5";
-
+import { Box } from '@radix-ui/themes';
 function Acceuil() {
   return (
     <div>
       <div className='header'>
+        <div className='stack grid grid-cols-2 bg-center lg:px-[200px] md:px-[100px]'>
+          <div className='content'>
+            <div>
+              <div className="bg-regal-green">
+                left
+              </div>
+              <div className="bg-primary-500">
+                right
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div>
         klqsjflmd
@@ -14,7 +26,7 @@ function Acceuil() {
         <br />
         jmslqkdjf
       </div>
-    </div>
+    </div >
   )
 }
 
