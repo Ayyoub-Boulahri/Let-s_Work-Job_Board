b4c0ff2909ac5b8f60546e5797e88170a4e3d6a7
AyoubPro44 <ayyoubboulahri@gmail.com>
2024-01-06 23:12:37 +0100

    signUp

---
+++ frontend/src/components/sign_up/AccountType.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Divider} from "@nextui-org/react";
+
+function AccountType() {
+  return (
+    <div>
+        <div>Employee</div>
+        <Divider className="my-4 bg-[#3D3D3D]" />
+        <div>Company</div>
+    </div>
+  )
+}
+
+export default AccountType
---
+++ frontend/src/components/sign_up/PersonnelInfosForm.jsx
@@ -0,0 +1,82 @@
+import React from 'react'
+import { useState } from 'react';
+import { Select, SelectItem } from "@nextui-org/react";
+
+function PersonnelInfosForm() {
+    const [phoneValue, setPhoneValue] = useState('ayyoub');
+
+    return (
+        <div className="slideshow flex flex-col gap-2">
+            <div className='input-group flex gap-4'>
+                <div>
+                    <label htmlFor="cin">CIN</label>
+                    <input type="text" id="cin" name="cin" placeholder='CIN' />
+                </div>
+
+                <div>
+                    <label htmlFor="firstName">First Name</label>
+                    <input type="text" id="firstName" name="firstName" placeholder='First Name' />
+                </div>
+
+                <div>
+                    <label htmlFor="lastName">Last Name</label>
+                    <input type="text" id="lastName" name="lastName" placeholder='Last Name' />
+                </div>
+            </div>
+
+            <div className='input-group flex gap-6 w-full'>
+                <div className='w-full'>
+                    <label htmlFor="phoneNumber">Phone Number</label>
+                    <input type="tel" id="phoneNumber" name="phoneNumber" placeholder='Phone Number' defaultValue={"+212 "} />
+                </div>
+
+                <div className='w-full'>
+                    <label htmlFor="dob">Date of Birth</label>
+                    <input type="date" id="dob" name="dob" />
+                </div>
+            </div>
+
+            <div className='input-group flex gap-6 w-full'>
+                <div className='w-full'>
+                    <label htmlFor="country">Country</label>
+                    <select id="country" name="country" className='select'>
+                        <option value="" disabled selected>Select a country</option>
+                        <option value="MAR">Morocco</option>
+                        <option value="USA">United States</option>
+                        <option value="CAN">Canada</option>
+                        <option value="GBR">United Kingdom</option>
+                        <option value="AUS">Australia</option>
+                        <option value="GER">Germany</option>
+                        <option value="FRA">France</option>
+                        <option value="JPN">Japan</option>
+                    </select>
+                </div>
+
+                <div className='w-full'>
+                    <label htmlFor="city">City</label>
+                    <select id="city" name="city" className='select'>
+                        <option value="" disabled selected>Select a city</option>
+                        <option value="MAR">Morocco</option>
+                        <option value="USA">United States</option>
+                        <option value="CAN">Canada</option>
+                        <option value="GBR">United Kingdom</option>
+                        <option value="AUS">Australia</option>
+                        <option value="GER">Germany</option>
+                        <option value="FRA">France</option>
+                        <option value="JPN">Japan</option>
+                    </select>
+                </div>
+            </div>
+
+            <div className='input-group'>
+                <div>
+                    <label htmlFor="address">Address</label>
+                    <input type="text" id="address" name="address" placeholder="Enter Your Address" />
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default PersonnelInfosForm
