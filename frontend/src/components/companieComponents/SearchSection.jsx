6e194b841106a70118dda597ffa18d862edd229a
khalaf67 <khalaf.drhourhi@gmail.com>
2024-01-13 00:11:12 +0100

    tst

---
+++ frontend/src/components/companieComponents/SearchSection.jsx
@@ -0,0 +1,79 @@
+import PropTypes from 'prop-types'
+import React, { Component } from 'react'
+
+export class SearchSection extends Component {
+ 
+  render() {
+    const ListOfCompanies = [
+        {
+          id: 1,
+          name: "Tech Innovators Inc.",
+          industry: "Technology",
+          founded_year: 2005,
+          hq_location: "Silicon Valley, CA",
+          website: "http://techinnovators.com"
+        },
+        {
+          id: 2,
+          name: "GreenEco Solutions",
+          industry: "Renewable Energy",
+          founded_year: 2010,
+          hq_location: "Austin, TX",
+          website: "http://greeneco.com"
+        },
+        {
+          id: 3,
+          name: "Global Pharma Solutions",
+          industry: "Pharmaceuticals",
+          founded_year: 1998,
+          hq_location: "New York, NY",
+          website: "http://globalpharma.com"
+        },
+        {
+          id: 4,
+          name: "Creative Minds Studio",
+          industry: "Design and Media",
+          founded_year: 2012,
+          hq_location: "Los Angeles, CA",
+          website: "http://creativemindsstudio.com"
+        },
+        {
+          id: 5,
+          name: "Foodie Ventures",
+          industry: "Food and Beverage",
+          founded_year: 2007,
+          hq_location: "Chicago, IL",
+          website: "http://foodieventures.com"
+        },
+        {
+          id: 6,
+          name: "CleanTech Solutions",
+          industry: "Environmental Services",
+          founded_year: 2015,
+          hq_location: "Seattle, WA",
+          website: "http://cleantechsolutions.com"
+        },
+        {
+          id: 7,
+          name: "Financial Wizards LLC",
+          industry: "Finance",
+          founded_year: 2000,
+          hq_location: "New York, NY",
+          website: "http://financialwizards.com"
+        }
+      ];
+      
+    return (
+      <section>
+        <div>
+            <h1>
+                Discover the available companies
+            </h1>
+
+        </div>
+      </section>
+    )
+  }
+}
+
+export default SearchSection
--- frontend/src/pages/Companies.jsx
+++ frontend/src/pages/Companies.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import styles from '../style'
+import Leading from '../components/companieComponents/Leading'
+import SearchSection from '../components/companieComponents/SearchSection'
+import { Divider} from "@nextui-org/react";
+
 function Companies() {
   return (
     <div className={`${styles.flexStart} pt-20 xl:pb-4`}>
       <div className={`${styles.boxWidth}`}>
-
-        <div>
-          companies
-        </div>
+        <Leading/>
+        <Divider className="my-4 bg-[#3D3D3D] sm:block hidden w-[80%] ml-24 " />
+        <SearchSection/>
       </div>
     </div>
   )
