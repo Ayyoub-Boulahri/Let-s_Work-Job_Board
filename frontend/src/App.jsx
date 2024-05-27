11831daad3811ef9144fd79ab8ea2f97b7824754
AyoubPro44 <ayyoubboulahri@gmail.com>
2024-05-27 16:35:10 +0100

    updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Profile from './pages/profile'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
 import Jobs from './pages/Jobs'
+import Companies from './pages/companies'
 import { Provider } from 'react-redux';
 import { authStore } from './stores/authStore'
 import Company from './pages/company'
@@ -49,41 +50,3 @@ function App() {
 }
 
 export default App
-
-
-import React, { useEffect, useState } from 'react'
-import styles from './style'
-import CompanyLeading from './components/companieComponents/CompanyLeading'
-import SearchSection from './components/companieComponents/SearchSection'
-import { Divider } from "@nextui-org/react";
-import { useNavigate } from 'react-router-dom';
-import handleLogout from './services/handleLogout';
-import { useDispatch} from 'react-redux';
-import { setLoginOut } from './stores/authStore';
-import "./css/height.css"
-
-function Companies() {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const [searchTxt, setSearchTxt] = useState("")
-
-  useEffect(() => {
-    const typeUser = localStorage.getItem('typeUser');
-    if (typeUser != "employee") {
-      handleLogout();
-      dispatch(setLoginOut());
-      navigate("/");
-    }
-  }, [])
-
-  return (
-    <div className={`${styles.flexStart} ${styles.paddingX} bg-section-dark-bg pt-20 xl:pb-4`}>
-      <div className={`${styles.boxWidth} HeightTall`}>
-        <CompanyLeading searchTxt={searchTxt} setSearchTxt={setSearchTxt} />
-        <Divider className="my-4 bg-[#3D3D3D] sm:block hidden" />
-        <SearchSection searchTxt={searchTxt} setSearchTxt={setSearchTxt} />
-      </div>
-    </div>
-  )
-}
-
